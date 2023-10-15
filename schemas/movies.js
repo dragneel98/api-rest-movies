226c28f90ac9dd2c5018ba8b739caadf68c33d8b
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2023-10-15 14:26:49 -0300

    fix exports

--- schemas/movies.js
+++ schemas/movies.js
@@ -21,15 +21,11 @@ const movieSchema = z.object({
   )
 })
 
-function validateMovie (input) {
+export function validateMovie(input) {
   return movieSchema.safeParse(input)
 }
 
-function validatePartialMovie (input) {
+export function validatePartialMovie(input) {
   return movieSchema.partial().safeParse(input)
 }
 
-module.exports = {
-  validateMovie,
-  validatePartialMovie
-}
